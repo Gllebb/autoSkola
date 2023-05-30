e067cf1f9e6596284814c2341a70e6b2d2de428d
Gllebb <myggithub@gmail.com>
2023-05-30 10:44:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ def MainMenu():
     print('''
 Driving school \"Splinter\"!
 ------------------------------------------
-Main menu: (Add colors)
+Main menu: (Add colors, main menu button)
 ------------------------------------------
 Select one of the options:
 ------------------------------------------
 1. Add data
 2. View data
 3. Delete data
-4. Search data ? (netu)
-5. Do math ?
-6. Sort ?
+4. Filter data
+5. Do math
+6. Sort
 7. Exit
 ------------------------------------------''')
     while True:
@@ -34,27 +34,28 @@ Select one of the options:
         print("Invalid input. Please enter a number between 1 and 7.")
     os.system("clear")
 
-    # new part
-
-    if (userChoise == 1):
-      print('''
+    print('''
 What type of data this will be?
 ------------------------------------------
 1. About Instructors
 2. About Cars
 3. About Students
     ''')
-      while True:
-        try:
-          userChoise1 = int(input("Input: "))
-          if (userChoise1 < 1 or userChoise1 > 3):
-            print("Number should be rounded between 1 and 3.")
-            continue
-          break
-        except ValueError:
-          print("Invalid input. Please enter a number between 1 and 3.")
-      os.system("clear")
-      if (userChoise1 == 1):
+    while True:
+      try:
+        userChoise1 = int(input("Input: "))
+        if (userChoise1 < 1 or userChoise1 > 3):
+          print("Number should be rounded between 1 and 3.")
+          continue
+        break
+      except ValueError:
+        print("Invalid input. Please enter a number between 1 and 3.")
+    os.system("clear")
+
+    # new part
+
+    if userChoise == 1:
+      if userChoise1 == 1:
         Instructor.inputInstructor()
       elif userChoise1 == 2:
         print("Work in progress...")
@@ -63,27 +64,10 @@ What type of data this will be?
       else:
         print("Something went wrong:(")
 
-# new part
+    # new part
 
-    elif (userChoise == 2):
-      print('''
-What type of data this will be?
-------------------------------------------
-1. About Instructors
-2. About Cars
-3. About Students
-    ''')
-      while True:
-        try:
-          userChoise1 = int(input("Input: "))
-          if (userChoise1 < 1 or userChoise1 > 3):
-            print("Number should be rounded between 1 and 3.")
-            continue
-          break
-        except ValueError:
-          print("Invalid input. Please enter a number between 1 and 3.")
-
-      if (userChoise1 == 1):
+    elif userChoise == 2:
+      if userChoise1 == 1:
         Instructor.printInstructors()
       elif userChoise1 == 2:
         print("Work in progress...")
@@ -92,42 +76,25 @@ What type of data this will be?
       else:
         print("Something went wrong:(")
 
-# new part
+    # new part
 
     elif (userChoise == 3):
       print('''
-What type of data this will be?
-------------------------------------------
-1. About Instructors
-2. About Cars ?
-3. About Students ?
-    ''')
-      while True:
-        try:
-          userChoise1 = int(input("Input: "))
-          if (userChoise1 < 1 or userChoise1 > 3):
-            print("Number should be rounded between 1 and 3.")
-            continue
-          break
-        except ValueError:
-          print("Invalid input. Please enter a number between 1 and 3.")
-      os.system("clear")
-      if userChoise1 == 1:
-        print('''
 1. Delete specific data
 2. Delete all the data
 ------------------------------------------
 ''')
-        while True:
-          try:
-            userChoise2 = int(input("Input: "))
-            if (userChoise2 < 1 or userChoise2 > 2):
-              print("Number should be rounded between 1 and 2.")
-              continue
-            break
-          except ValueError:
-            print("Invalid input. Please enter a number between 1 and 2.")
-        os.system("clear")
+      while True:
+        try:
+          userChoise2 = int(input("Input: "))
+          if (userChoise2 < 1 or userChoise2 > 2):
+            print("Number should be rounded between 1 and 2.")
+            continue
+          break
+        except ValueError:
+          print("Invalid input. Please enter a number between 1 and 2.")
+      os.system("clear")
+      if userChoise1 == 1:
         if userChoise2 == 1:
           print('''
 1. Serch by ID
@@ -160,47 +127,66 @@ What type of data this will be?
       else:
         print("Something went wrong:(")
 
-# new part
+    # new part
 
     elif (userChoise == 4):
-      print('''
-What type of data you want to search?
-------------------------------------------
-1. About Instructors
-2. About Cars
-3. About Students
-''')
-      userChoise = int(input("Enter there: "))
-      print("Work in progress!")
+      if userChoise1 == 1:
+        Instructor.filterInstructor()
+      elif userChoise1 == 2:
+        print("Work in progress...")
+      elif userChoise1 == 3:
+        print("Work in progress...")
+      else:
+        print("Something went wrong:(")
 
-# new part
+    # new part
 
     elif (userChoise == 5):
-      print('''
+      if userChoise1 == 1:
+        print('''
 1. Show Work expirience before applying to our job
 2. Show at what age Instructor applied to his first job
-3. Show amounth of years to his next anniversary''')
-      userChoise = int(input("Enter there: "))
-      Instructor.doMathInstructor(userChoise)
+3. Show amounth of years to his next anniversary
+''')
+      while True:
+        try:
+          userChoise2 = int(input("Input: "))
+          if (userChoise2 < 1 or userChoise2 > 3):
+            print("Number should be rounded between 1 and 3.")
+            continue
+          break
+        except ValueError:
+          print("Invalid input. Please enter a number between 1 and 3.")
+      os.system("clear")
+      Instructor.doMathInstructor(userChoise2)
 
-# new part
+    # new part
 
     elif (userChoise == 6):
-      print('''
+      if userChoise1 == 1:
+        print('''
 1. Sort People by the age from smallest to biggest
 2. Sort People by the age from biggest to smallest
 3. Sort People by the name from A to Z
 4. Sort People by the name from Z to A
-          ''')
-      userChoise = int(input("Enter there: "))
-      print("---------------------------------")
-      Instructor.sortingInstructor(userChoise)
+''')
+        while True:
+          try:
+            userChoise2 = int(input("Input: "))
+            if (userChoise2 < 1 or userChoise2 > 4):
+              print("Number should be rounded between 1 and 4.")
+              continue
+            break
+          except ValueError:
+            print("Invalid input. Please enter a number between 1 and 4.")
+        os.system("clear")
+        Instructor.sortingInstructor(userChoise2)
 
-
-# new part
+    # new part
 
     elif (userChoise == 7):
       print("See you next time!")
       break
+
 
 MainMenu()
